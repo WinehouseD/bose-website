0d7c5a22048abd088f74556b487338a411e91cad
Diana <kitdiana6@gmail.com>
2023-08-22 18:40:54 +0300

    scholl-top, scholl-bar

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", function () {
+  var scroll = document.querySelector(".scroll-top");
+  scroll.classList.toggle("active", window.scrollY > 500);
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+  });
+}
